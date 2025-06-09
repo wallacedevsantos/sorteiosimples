a6ec474797a8b066325afcd03bf83383ef88f966
wallacedevsantos <wallacedev.oliveirasantos@gmail.com>
2025-06-09 09:20:44 -0300

    atualização e deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sorteiosimples/' : '/',
   server: {
     host: "::",
     port: 8080,
